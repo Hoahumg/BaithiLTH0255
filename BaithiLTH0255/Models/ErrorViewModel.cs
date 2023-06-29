c213359f99b45b08178fc77bddc51ff07a6906e3
Hoahumg <lethihoa18032001@gamil.com>
2023-06-29 08:48:27 +0700

    Hoa

---
+++ BaithiLTH0255/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace BaithiLTH0255.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
